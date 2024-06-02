24478ae52f9036fab23d078312a8a869c0fe44aa
PAVAN5221 <pavan123199@gmail.com>
2024-06-02 09:52:52 +0530

    Login page of swag labs

---
+++ login.java
@@ -0,0 +1,22 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver","D:\\pridehub\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+
+	}
+
+}
